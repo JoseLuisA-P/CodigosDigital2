4bd39431a727cee460e0393e77c966e335431b30
JoseLuisA-P <alv19392@uvg.edu.gt>
2021-08-04 18:29:42 -0600

    LAB4 I2C
    implementado el I2C

---
+++ Lab4/Master/Lab4M.X/main.c
@@ -0,0 +1,76 @@
+/* 
+ * ESTE ES EL CODIGO DEL MASTER
+ * File:   main.c
+ * Author: Jose Alvarez (19392)
+ * 
+ * Creado 4 de Agosto de 2021
+ * 
+ */
+//******************************************************************************
+//  Palabras de configurafcion e inclusion de librerias
+//******************************************************************************
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "I2C.h"
+#define _XTAL_FREQ 8000000 //utilizado para los delays
+
+//******************************************************************************
+//  Variables y prototipos de funciones
+//******************************************************************************
+uint8_t DatS1;
+void config(void);
+//******************************************************************************
+//  Rutina de interrupcion
+//******************************************************************************
+void __interrupt() interrupcion(void){
+    
+}
+
+//******************************************************************************
+//  loop principal
+//******************************************************************************
+void main(void) {
+    config();
+    while(1){  
+        MasterStart_I2C();//Inicia la comunicacion
+        MasterSend_I2C(0X21);//direccion del que se va a leer
+        MasterReceive_I2C(&DatS1);
+        MasterStop_I2C();//termina la comunicacion
+        __delay_ms(100);
+        
+        PORTD = DatS1;
+    }
+}
+
+//******************************************************************************
+//  funciones secundarias
+//******************************************************************************
+void config(void){
+    ANSEL =     0X00;
+    ANSELH =    0X00;
+    TRISA =     0X00;
+    TRISB =     0X00;
+    TRISD =     0X00;
+    PORTA =     0X00;
+    PORTB =     0X00;
+    PORTD =     0X00;
+
+    MasterInit_I2C(100000); //inicializado a 100KHz, frec estandar
+}
---
+++ Lab4/Slave1/LAB4S1.X/main.c
@@ -0,0 +1,111 @@
+/* 
+ * ESTE ES EL CODIGO DEL SLAVE 1
+ * File:   main.c
+ * Author: Jose Alvarez (19392)
+ * 
+ * Creado 4 de Agosto de 2021
+ * 
+ */
+//******************************************************************************
+//  Palabras de configurafcion e inclusion de librerias
+//******************************************************************************
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "I2C.h"
+#include "ADC.h"
+#define _XTAL_FREQ 8000000 //utilizado para los delays
+
+//******************************************************************************
+//  Variables y prototipos de funciones
+//******************************************************************************
+uint8_t clean; //dummy usada para limpiar el buffer
+uint8_t DATA; //datos que se enviaran
+void config(void);
+//******************************************************************************
+//  Rutina de interrupcion
+//******************************************************************************
+void __interrupt() interrupcion(void){
+    
+    if(PIR1bits.SSPIF){
+    
+        SSPCONbits.CKP = 0; //mantiene el clock en 0
+        //proteccion por si existe una colision
+        if ((SSPCONbits.SSPOV) || (SSPCONbits.WCOL)){
+            clean = SSPBUF; //lee el dato previo que se escribio en el buffer
+            SSPCONbits.SSPOV = 0; //luego de leer indica que no hay overflow
+            SSPCONbits.WCOL = 0; //si existe colision la elimina
+            SSPCONbits.CKP = 1;  //habilita el clock de nuevo
+        }
+        
+        if(!SSPSTATbits.R && !SSPSTATbits.D){ //mira si es dato y si escribe
+            clean = SSPBUF; //limpia el buffer
+            SSPCONbits.CKP = 1;//Habilita el clock de nuevo
+            while(!SSPSTATbits.BF);//espera a que se llene el buffer
+            PORTB = SSPBUF;
+            __delay_ms(1);
+        }
+        
+        else if(SSPSTATbits.R && !SSPSTATbits.D){
+            clean = SSPBUF; //limpia el buffer
+            SSPSTATbits.BF = 0;
+            SSPBUF = DATA; //Coloca el dato en el buffer
+            SSPCONbits.CKP = 1; //Habilita el clock de nuevo
+            __delay_ms(1);
+            while(SSPSTATbits.BF); //espera a que lo envie
+        }
+        
+        PIR1bits.SSPIF = 0; 
+    
+    }
+    
+}
+
+//******************************************************************************
+//  loop principal
+//******************************************************************************
+void main(void) {
+    config();
+    while(1){
+   
+    }
+}
+
+//******************************************************************************
+//  funciones secundarias
+//******************************************************************************
+void config(void){
+    ANSEL =     0X01;
+    ANSELH =    0X00;
+    TRISA =     0X01;
+    TRISB =     0X00;
+    TRISD =     0X00;
+    PORTA =     0X00;
+    PORTB =     0X00;
+    PORTD =     0X00;
+    
+    ADCconfig(0,0);
+    
+    INTCONbits.GIE = 1; //Interrupciones usadas por el I2C
+    INTCONbits.PEIE = 1;
+    PIR1bits.SSPIF = 0;
+    PIE1bits.SSPIE = 1;
+    
+    SlaveInit_I2C(0X20);
+}
