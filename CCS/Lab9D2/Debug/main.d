1fdb7f971eb537cf36f11285d567b27a6c0ecc8b
JoseLuisA-P <alv19392@uvg.edu.gt>
2021-10-30 15:49:45 -0600

    configuracion timer0

--- CCS/Lab9D2/Debug/main.d
+++ CCS/Lab9D2/Debug/main.d
@@ -12,10 +12,13 @@ main.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine
 main.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
 main.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
 main.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
 
 ../main.c:
 
@@ -41,11 +44,17 @@ C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
 
 C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h:
+
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h:
 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h:
+
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h:
 
--- CCS/Lab9D2/main.c
+++ CCS/Lab9D2/main.c
@@ -6,13 +6,18 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "inc/hw_ints.h"
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
+#include "driverlib/timer.h"
 
 //Variables de control
-bool deb1;
+uint8_t deb1; //para el antirebote
+uint8_t i; //para el ciclo for
+uint32_t valTimer; //valor a cargar en el timer
 
 //PROTOTIPOS
 void delayMs(uint32_t milis);
@@ -20,17 +25,33 @@ void combinacionColores(void);
 
 int main(void)
 {   //configuracion de los perifericos y reloj de la placa
-    SysCtlClockSet(SYSCTL_SYSDIV_8 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //Reloj a 25MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //Reloj a 40MHz
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //reloj para el puerto F habilitado
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3); //pines del puerto F como salidas ROJO/AZUL/VERDE
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE,GPIO_PIN_4); //Push 1 como entrada
     GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU); //pin4 indicado como un pull-up
 
-    while(1){ //LOOP para ejecutar el programa
-        if(!GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)){
-        combinacionColores();
+    //configuracion del timmer
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0); //habilitar el reloj del timmer 0
+    TimerConfigure(TIMER0_BASE,TIMER_CFG_PERIODIC); //configurar el Timer0 como periodico y down, de 32 bits
+    TimerLoadSet(TIMER0_BASE,TIMER_BOTH,valTimer);
+    IntEnable(INT_TIMER0B); // Habilita la interrupcion del timer0a
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMB_TIMEOUT); //establece interrupcion por timeout en el timer 0
+    IntMasterEnable(); //habilita las interrupciones globales
+    TimerEnable(TIMER0_BASE,TIMER_BOTH); //para usar ambos timers0
 
+    while(1){ //LOOP para ejecutar el programa
+
+        //antirebote para el semaforo
+        if(!GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)){
+            deb1 = 1;
         }
+
+        if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4) && deb1 == 1 ){
+            deb1 = 0;
+            combinacionColores();
+        }
+
     }
 
 }
@@ -44,11 +65,19 @@ void delayMs(uint32_t milis){
 
 //rutina para mostrar cada color de forma individual PRELAB
 void combinacionColores(void){
-    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X02); //enciende el rojo
-    delayMs(1000);
-    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X0A); //enciende el amarillo
-    delayMs(1000);
+    //rutina para mostrar el movimiento del semaforo
     GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X08); //enciende el verde
-    delayMs(1000);
-    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X00);
+    delayMs(2000);
+    for(i=0; i< 9; i++){
+        if(i%2 == 0){
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X08); //enciende el verde
+        }
+        else{
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X00); //apaga las luces
+        }
+       delayMs(400);
+    }
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X0A); //enciende el amarillo
+    delayMs(2000);
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3,0X02); //enciende el rojo
 }
